a9de5dffb59b7fc1db2a457482a3dd833ab23b65
seungmai <76721552+seungmai@users.noreply.github.com>
2021-01-23 17:19:18 +0900

    JS: 연습

---
+++ 1.js
@@ -0,0 +1,6 @@
+'use strict' //자바스크립트 시작할 때 무조건 !!!
+
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(log)
+})
--- practice.html/main.js
+++ practice.html/main.js
@@ -1,17 +1,28 @@
-'use strict' //자바스크립트 시작할 때 무조건 !!!
+'use strict'
 
-
-// Make navabar transparent when it is on the top
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     console.log(window.scrollY);
-    // scrollY를 쓰면 스크롤 포지션을 알 수 있다.
     console.log(`navbarHeight: ${navbarHeight}`);
-    // navbarHeight이 104로 나오게 된다.
-    if(window.scrollY > navbarHeight) {
+    if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
-    } else{
+    } else {
         navbar.classList.remove('navbar--dark');
     }
+}); 
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link === null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
 });
