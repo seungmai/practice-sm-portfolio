ebd2d7033cb73bf09db33c70c61f906776358f3e
seungmai <76721552+seungmai@users.noreply.github.com>
2021-01-24 17:09:01 +0900

    JS: 연습

--- practice.html/main.js
+++ practice.html/main.js
@@ -18,11 +18,20 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     console.log(event.target.dataset.link);
     const target = event.target;
-    const link = target.dataset.link;
+    const link = target.dataset.link;``
     if (link === null){
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact')
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
