77dd83ce8cb8db52ac74e2029cec7b579261593c
seungmai <76721552+seungmai@users.noreply.github.com>
2021-01-25 12:14:13 +0900

    JS: Home contact button

--- practice.html/main.js
+++ practice.html/main.js
@@ -13,6 +13,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
@@ -20,7 +21,16 @@ navbarMenu.addEventListener('click', (event) => {
     if (link === null) {
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
