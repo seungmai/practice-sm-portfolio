d7adb42597351cea14a552c875b9e7568204a022
seungmai <76721552+seungmai@users.noreply.github.com>
2021-01-25 12:02:01 +0900

    JS: Navbar menu click

--- practice.html/main.js
+++ practice.html/main.js
@@ -7,8 +7,20 @@ document.addEventListener('scroll', () => {
     console.log(window.scrollY);
     console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
+        navbar.classList.add ('navbar--dark'); //classList는 add와 remove메소드를 이용하여 변형할 수 있다.
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link === null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
 });
