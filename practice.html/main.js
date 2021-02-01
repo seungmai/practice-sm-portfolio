fbe96406e1b4dec20e6930d393d6c287aa2bf3c8
seungmai <76721552+seungmai@users.noreply.github.com>
2021-02-01 17:08:54 +0900

    JS: navbar 정리

--- practice.html/main.js
+++ practice.html/main.js
@@ -1,15 +1,15 @@
-'use strict'
+'use strict' //use strict을 사용해서 우리가 미친 짓을 못 하도록 미연에 방지를 해준다.
 
-// Make navbar transparent when it is on the top
-const navbar = document.querySelector ('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
-    if(window.scrollY > navbarHeight) {
-        navbar.classList.add ('navbar--dark'); //classList는 add와 remove메소드를 이용하여 변형할 수 있다.
+// Make navbar transparent when it is on the top(navbar를 투명하게 만들건데 올라가면 투명 내려가면 색깔있는 걸로!)
+const navbar = document.querySelector ('#navbar'); // 변수 navbar를 선언한 다음에 document에 있는 querySelector를 이용해서 우리의 앨리먼트 요소를 받아온다. html에 아이디로 되어있기 때문에 #navbar로 적어준다.
+const navbarHeight = navbar.getBoundingClientRect().height; //#navbar 그리고 navbar의 height을 하려면 querySelector를 이용하게 되면 컨트롤 또는 커맨드를 이용해서 클릭하면 함수가 정의된 곳으로 갈 수 있다. 그래서 navbar에 querySelector를 이용해서 가져온 navbar요소의 높이를, 변수를 이용해서 할당한다. navbar에게 getBoundingClientRect()을 호출하면 여기안에 height가 있다.
+document.addEventListener('scroll', () => { //EventListener를 document에 먼저 스크롤이 될 때마다 나에게 우리가 등록한 이 함수를 호출해줘! ()이것은 Arrow function이라고하는데, 즉 아무런 인자를 받지않고 우리가 원하는 블럭을 실행하는 것이다. 그래서 스크롤이 될 때마다 여기 블럭안에서 우리가 작성한 코드가 실행 되어지도록 하는 것이다.
+    console.log(window.scrollY); // console.log에 window에 scrollY를 쓰면 스크롤 포지션을 알 수 있다.
+    console.log(`navbarHeight: ${navbarHeight}`); // 높이를 지정할 때는 navbarHeight처럼 이름을 설정하고 백틱가호를 이용해서 사용하는 것이 좋다.
+    if(window.scrollY > navbarHeight) { // window에 scrollY가 우리 navbar height보다 크면 즉 스크롤링이 navbar height 이상으로 스크롤링이 발생하면~ 
+        navbar.classList.add ('navbar--dark'); //우리가 navbar에 있는 클래스 리스트를 추가(add)해 줄건데 navbar가 이루어지면 navbar의 dark.BEM의 Modifier를 이용해서 navbar가 어두어지게 만든다. 그래서 스크롤링이 클 때만 navbar--dark를 추가한다. classList는 add와 remove메소드를 이용하여 변형할 수 있다.
     } else {
-        navbar.classList.remove('navbar--dark');
+        navbar.classList.remove('navbar--dark'); //스크롤링이 navbar height보다 작으면, 즉 스크롤링이 많이 되지 않았으면 navbar--dark를 없애줄 것이다.
     }
 });
 
